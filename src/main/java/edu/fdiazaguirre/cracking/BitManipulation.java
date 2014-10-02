9a0cd624e14b6ee8be053ab369a293bd42662870
fdiazaguirre <fdiazaguirre@gmail.com>
2014-10-02 14:49:55 -0700

    BitManipulation tests

--- src/main/java/edu/fdiazaguirre/cracking/BitManipulation.java
+++ src/main/java/edu/fdiazaguirre/cracking/BitManipulation.java
@@ -1,38 +1,92 @@
 package edu.fdiazaguirre.cracking;
 
+import java.math.RoundingMode;
+
+import com.google.common.base.Preconditions;
+import com.google.common.math.IntMath;
+
 public class BitManipulation {
 
 	/**
 	 * 5.1
+	 * Insert m into n such that m starts at bit j and ends at bit i.
 	 */
-	public static int updateBits(int n, int m, int i, int j) {
-		int result = 0;
-		int holder = m & getMask(i, j);
-		int shifted = 0 << i;
-		result = holder | shifted;
-		return result;
+	public static int insertBinary(int n, int m, int highIndex, int lowIndex) {
+		Preconditions.checkArgument(Integer.toBinaryString(n).length() >= (highIndex - lowIndex + 1));
+		// clear m from highIndex to 0
+		int nCleared = n & clearBitsIthrough0(n, highIndex);
+		// restore right digits
+		// Using unsigned shift to restore the wrong cleared bits.
+		// This approach is more readable and faster than avoid clear the lowIndex - 1 to 0 bits. 
+		int trailingOnes = lowIndex - 1;
+		int leadingZeros = 32 - trailingOnes ;
+		int rightMask = -1 >>> leadingZeros;
+		nCleared = nCleared | rightMask;
+		// move n to start at highIndex
+		int mShifted =  m << lowIndex;
+		return nCleared | mShifted;
 	} 
 	
+	public static int clearBitsIthrough0(int num, int highIndex) {
+		int mask = ~(( 1 << (highIndex + 1)) - 1);
+		return num & mask;
+	}
+	
 	/**
-	 * 5.1 
+	 * Creates a mask for the Integer.MAX_VALUE binary representation clearing
+	 * the values between leftIndex and rightIndex.
+	 * 
+	 * @param leftMostIndex
+	 *            the far left index to be allowed.
+	 * @param rightMostIndex
+	 *            the far right index to be allowed.
 	 */
-	public static int getMask(int i, int j) {
-		int allOnes = ~0;
-		int left = allOnes << (j+1);
-		int right = ((1 << i) - 1);
+	public static int getMask(int leftMostIndex, int rightMostIndex) {
+		int allOnes = Integer.parseInt(
+				Integer.toBinaryString(Integer.MAX_VALUE), 2);
+		// "<<" is a signed operation for this reason use "~" to revert the sign
+		// effect in the left most digit
+		int left = ~(allOnes << (rightMostIndex + 1));
+		int right = ((1 << leftMostIndex) - 1);
 		return left | right;
 	}
 	
 	/**
 	 * 5.2
 	 */
-	public static void printNumber(double d) {
+	public static String printNumber(double d) {
+		int partialResult = 0;
 		if (d > Integer.MAX_VALUE) {
-			System.out.println("ERROR!");
+			throw new RuntimeException("ERROR");
 		} else {
-			//TODO: what I should do after?
-			int digits = Integer.bitCount((int) d);
-			System.out.println(digits);
+			// get msb and save it in number
+			int exponent = IntMath.log2(Double.valueOf(d).intValue(), RoundingMode.HALF_EVEN);
+			partialResult = 1 << exponent;
+			// get remainder = number - pow(2, exponent)
+			int remainder = Double.valueOf(d).intValue() - IntMath.pow(2, exponent);
+			while (remainder > 0) {
+				// get exponent
+				exponent = IntMath.log2(Double.valueOf(remainder).intValue(), RoundingMode.HALF_EVEN);
+				// update bits
+				partialResult  = BitManipulation.setBit(partialResult, exponent);
+				// compute new value for remainder
+				remainder = Double.valueOf(remainder).intValue() - IntMath.pow(2, exponent);
+			}
 		}
+		return Integer.toBinaryString(partialResult);
+	}
+
+	public static int setBit(int number, int index) {
+		return number | (1 << index);
+	}
+	
+	public static int setBits(int number, int highIndex, int lowIndex) {
+		Preconditions.checkArgument(highIndex >= lowIndex);
+		Preconditions.checkArgument(lowIndex >= 0);
+		int result = 0;
+		for (int i = lowIndex; i <= highIndex; i++) {
+			result = result | (1 << i);
+		}
+		return number | result;
 	}
 }
--- src/test/java/edu/fdiazaguirre/bitmanipulation/BitManipulationTest.java
+++ src/test/java/edu/fdiazaguirre/bitmanipulation/BitManipulationTest.java
@@ -1,8 +1,7 @@
 package edu.fdiazaguirre.bitmanipulation;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import edu.fdiazaguirre.cracking.BitManipulation;
@@ -35,35 +34,110 @@ public class BitManipulationTest {
 		c = a >> 2; /* 15 = 1111 */
 		assertEquals(15, c);
 
-		//The unsigned right shift operator ">>>" shifts a zero into the leftmost position,
+		// The unsigned right shift operator ">>>" shifts a zero into the
+		// leftmost position,
 		// while the leftmost position after ">>" depends on sign extension.
 		c = a >>> 2; /* 15 = 0000 1111 */
 		assertEquals(15, c);
 		c = b >>> 1;
 		assertEquals(6, c); /* 1101 >>> 1 = 0110 */
-		
+
 		int bitmask = 0x000F;
 		int val = 0x2222;
 		assertEquals(2, val & bitmask);
 	}
 
 	@Test
-	public void testMask() {
-		// -15 = 110001
-		assertEquals(-15, BitManipulation.getMask(1, 3));
+	public void testClearBitsIthrough0() {
+		int expected = Integer.parseInt("11111110000", 2);
+		int n = Integer.parseInt("11111111111", 2);
+
+		assertEquals(expected, BitManipulation.clearBitsIthrough0(n, 3));
 	}
-	
-	@Ignore
+
 	@Test
-	public void testUpdateBits() {
-		// 17 = 010011
-		assertEquals(17, BitManipulation.updateBits(2, 15, 1, 2));
+	public void testShift() {
+		assertEquals("1",
+				Integer.toBinaryString(Integer.parseInt("1111", 2) >> 3));
 	}
-	
-	@Ignore
+
+	@Test
+	public void testSetBit() {
+		int expected = Integer.parseInt("1101", 2);
+		int original = Integer.parseInt("0101", 2);
+
+		assertEquals(Integer.toBinaryString(expected),
+				Integer.toBinaryString(BitManipulation.setBit(original, 3)));
+	}
+
+	@Test
+	public void testSetBits() {
+		int expected = Integer.parseInt("101111", 2);
+		int original = Integer.parseInt("100000", 2);
+
+		assertEquals(Integer.toBinaryString(expected),
+				Integer.toBinaryString(BitManipulation.setBits(original, 3, 0)));
+
+		expected = Integer.parseInt("101110", 2);
+		original = Integer.parseInt("100000", 2);
+
+		assertEquals(Integer.toBinaryString(expected),
+				Integer.toBinaryString(BitManipulation.setBits(original, 3, 1)));
+	}
+
+	@Test
+	public void testMask() {
+		assertEquals(Integer.parseInt("1111", 2), BitManipulation.getMask(1, 3));
+	}
+
+	@Test
+	public void testinsertBinary() {
+		int expected = Integer.parseInt("10001001101", 2);
+		int m = Integer.parseInt("10011", 2);
+		int n = Integer.parseInt("10000000001", 2);
+		int highIndex = 6;
+		int lowIndex = 2;
+
+		assertEquals(Integer.toBinaryString(expected),
+				Integer.toBinaryString(BitManipulation.insertBinary(n, m,
+						highIndex, lowIndex)));
+	}
+
+	@Test
+	public void testRightShift() {
+		int expected = Integer.parseInt("00000000111111111111111111111111", 2);
+
+		assertEquals(Integer.toBinaryString(expected),
+				Integer.toBinaryString(-1 >>> 8));
+		int trailingOnes = 2;
+		int leadingZeros = 32 - trailingOnes;
+		expected = Integer.parseInt("00000000000000000000000000000011", 2);
+
+		assertEquals(Integer.toBinaryString(expected),
+				Integer.toBinaryString(-1 >>> leadingZeros));
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testPrintNumberThrowsException() {
+		double d = Double.MAX_VALUE;
+		BitManipulation.printNumber(d);
+	}
+
 	@Test
 	public void testPrintNumber() {
-		double d = 25;
-		assertEquals(4, Integer.highestOneBit((int) d));
+		Double d = new Double(8.0d);
+
+		assertEquals(Integer.toBinaryString(Integer.parseInt("1000", 2)),
+				BitManipulation.printNumber(d));
+
+		d = new Double(10.0d);
+
+		assertEquals(Integer.toBinaryString(Integer.parseInt("1010", 2)),
+				BitManipulation.printNumber(d));
+		
+		d = new Double(72.0d);
+
+		assertEquals(Integer.toBinaryString(Double.valueOf(d).intValue()),
+				BitManipulation.printNumber(d));
 	}
 }
